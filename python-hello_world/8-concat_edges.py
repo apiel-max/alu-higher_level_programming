6e139fb3a218e0fa7b942656d1329a273a569836
apiel_max <a.piel@alustudent.com>
2026-02-20 14:29:29 -0800

    python files

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an object-oriented programming language with unmatched versatility"
-str = str[39:66] + str[106:112] + str[0:6]
+str = str[17:56] + str[51:57] + str[0:6]
 print(str)
