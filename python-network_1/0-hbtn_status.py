24fba300107edcb825324a7c35c58f5b8cb8d904
apiel_max <a.piel@alustudent.com>
2026-03-23 01:22:20 +0200

    Fix PEP8 on 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,8 +2,8 @@
 """Script that fetches https://alu-intranet.hbtn.io/status using urllib"""
 import urllib.request
 
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
-    body = response.read()
+with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as r:
+    body = r.read()
     print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
