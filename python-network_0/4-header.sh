8aff9efb342dd71c38dc6e2e2852e46fdb438d8b
apiel_max <a.piel@alustudent.com>
2026-03-31 15:36:22 +0200

    Fix 4-header.sh: use X-School-User-Id header; fix 2-create_read_user.sql: SELECT only

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with header X-HolbertonSchool-User-Id value 98
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request with header X-HolbertonSchool-User-Id set to 98 and displays the body
+curl -s -H "X-School-User-Id: 98" "$1"
