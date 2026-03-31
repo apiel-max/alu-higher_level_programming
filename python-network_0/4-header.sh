d8980894107cbdae3ff70ab4f4e07bd3341b3cae
apiel_max <a.piel@alustudent.com>
2026-03-31 15:27:24 +0200

    Fix 4-header.sh: hardcode header value 98 as per task

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL with header X-HolbertonSchool-User-Id value 98
-curl -s -H "X-HolbertonSchool-User-Id: $2" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
