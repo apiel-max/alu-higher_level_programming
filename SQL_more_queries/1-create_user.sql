8f6a9d3c864b6df262cf069c98ad075648496d2e
apiel_max <a.piel@alustudent.com>
2026-03-31 15:17:40 +0200

    Fix 1-create_user.sql: add WITH GRANT OPTION, remove FLUSH PRIVILEGES

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- Creates user user_0d_1 with all privileges on the server
+-- Creates MySQL user user_0d_1 with all privileges on the server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
