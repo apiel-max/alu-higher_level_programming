0f31e54ac9dc138f928dde51991ee6a30322ec7d
apiel_max <a.piel@alustudent.com>
2026-03-31 15:12:08 +0200

    Fix SQL_more_queries: fix privileges and user creation scripts

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Create user user_0d_1 with all privileges
+-- Creates user user_0d_1 with all privileges on the server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
