ec5abfa72cba07f1ee3d3623efbcc1731d8b2812
apiel_max <a.piel@alustudent.com>
2026-03-31 15:03:56 +0200

    Fix 0-privileges.sql: only show grants, remove create/grant statements

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,3 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE IF NOT EXISTS user_2_db;
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
-FLUSH PRIVILEGES;
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
