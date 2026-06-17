45932ded8b9c4e6a7fff3d3b5a4d5c89a6c89f6c
apiel_max <a.piel@alustudent.com>
2026-06-17 23:02:12 +0200

    add all javascripts files

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (isNaN(n) || n === 0) {
+  if (isNaN(n) || n <= 0) {
     return 1;
   }
   return n * factorial(n - 1);
 }
-console.log(factorial(parseInt(process.argv[2])));
+const n = parseInt(process.argv[2]);
+console.log(factorial(n));
